70b86e356c9b7b5858fa83e9620256e1af4b3e8c
ducmai <duc.mai@dnse.com.vn>
2024-04-10 10:12:37 +0700

    First commit

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Jack's Blog</title>
+      <title>Mike's Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/logo.png" />
     </>
   );
 }
